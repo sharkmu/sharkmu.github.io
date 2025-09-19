92f5d2018c1828dfff8590cf61f0dedd59c2ca43
sharkmu <csonka.samuel@gmail.com>
2025-09-19 21:31:48 +0200

    finish mobile nav bar

--- v2/js/script.js
+++ v2/js/script.js
@@ -1,6 +1,8 @@
 mobileMenuNavBar = document.getElementById("nav-bar-mobile-view");
+mobileMenuLeftIcons = document.getElementById("nav-bar-mobile-ellipses");
 
 function mobileMenu() {
     mobileMenuNavBar.classList.toggle("showMobileMenu");
+    mobileMenuLeftIcons.classList.toggle("showMobileMenu");
 
 }
